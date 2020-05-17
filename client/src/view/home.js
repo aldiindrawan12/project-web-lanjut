808775ebf2fd613cdeb1c95337f81b2860e11235
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2020-05-17 10:03:32 +0700

    rapihin
    add

--- client/src/view/home.js
+++ client/src/view/home.js
@@ -65,9 +65,15 @@ import {
             </div>
 
 
+            <div className="present">
+                <h1>Our Products</h1>
+            </div>
+
+
 
             
         <div className="container-home">
+
             <div className="tipe">
             <Card>
             <CardImg  src={pria} alt="Card image cap" />
@@ -77,28 +83,28 @@ import {
                 <Button color="primary" a href='/product/pria'>Yuk Cari</Button>
             </CardBody>
             </Card>
-        </div>
-                   
-        <div className="tipe">
-            <Card>
-            <CardImg  src={wanita} alt="Card image cap" />
-            <CardBody>
-            <div className="tipe-judul">Woman Apparels</div>
-                <CardText>Dapatkan diskon menarik dari koleksi katalog kami</CardText>             
-                 <Button color="primary" a href='/product/wanita'>Yuk Cari</Button>
-            </CardBody>
-            </Card>    
-        </div>
-        <div className="tipe">
-            <Card>
-            <CardImg  src={anak} alt="Card image cap" />
-            <CardBody>
-            <div className="tipe-judul">Kids Apparels</div>
-                <CardText>Segala Jenis Fashion Anak Tersedia di Sini</CardText>             
-                 <Button color="primary" a href='/product/anak-anak'>Yuk Cari</Button>
-            </CardBody>
-            </Card> 
             </div>
+                   
+            <div className="tipe">
+                <Card>
+                <CardImg  src={wanita} alt="Card image cap" />
+                <CardBody>
+                <div className="tipe-judul">Woman Apparels</div>
+                    <CardText>Dapatkan diskon menarik dari koleksi katalog kami</CardText>             
+                    <Button color="primary" a href='/product/wanita'>Yuk Cari</Button>
+                </CardBody>
+                </Card>    
+            </div>
+            <div className="tipe">
+                <Card>
+                <CardImg  src={anak} alt="Card image cap" />
+                <CardBody>
+                <div className="tipe-judul">Kids Apparels</div>
+                    <CardText>Segala Jenis Fashion Anak Tersedia di Sini</CardText>             
+                    <Button color="primary" a href='/product/anak-anak'>Yuk Cari</Button>
+                </CardBody>
+                </Card> 
+                </div>
         </div>
   
         </>
